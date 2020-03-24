a0306e177852331599a7f790889101ae2b090166
KiHyeon Song <53826792+KiHyeonSong@users.noreply.github.com>
2020-03-24 19:21:30 +0900

    UI

---
+++ app/src/main/java/com/tzutalin/dlibtest/menuEvent.java
@@ -0,0 +1,59 @@
+package com.tzutalin.dlibtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class menuEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        Button btn_menu_1 =(Button)findViewById(R.id.menu_1);
+        Button btn_menu_2 =(Button)findViewById(R.id.menu_2);
+        Button btn_menu_3 =(Button)findViewById(R.id.menu_3);
+        Button btn_menu_4 =(Button)findViewById(R.id.menu_4);
+        Button btn_menu_5 =(Button)findViewById(R.id.menu_5);
+
+        btn_menu_1.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent = new Intent(menuEvent.this, cameraEvent.class);
+               startActivity(intent);
+           }
+        });
+        btn_menu_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menuEvent.this, cameraEvent.class);
+                startActivity(intent);
+            }
+        });
+        btn_menu_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menuEvent.this, cameraEvent.class);
+                startActivity(intent);
+            }
+        });
+        btn_menu_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menuEvent.this, cameraEvent.class);
+                startActivity(intent);
+            }
+        });
+        btn_menu_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(menuEvent.this, cameraEvent.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
