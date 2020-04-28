efb49c029e08620519965639746e36b2c2bf1e45
KiHyeonSong <rlgusl8642@naver.com>
2020-04-28 13:10:46 +0900

    04_28 Dialog_Box

--- app/src/main/java/com/tzutalin/dlibtest/CameraActivity.java
+++ app/src/main/java/com/tzutalin/dlibtest/CameraActivity.java
@@ -17,18 +17,27 @@
 package com.tzutalin.dlibtest;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 /**
  * Created by darrenl on 2016/5/20.
  */
 public class CameraActivity extends Activity {
+
 
     private static int OVERLAY_PERMISSION_REQ_CODE = 1;
 
@@ -38,6 +47,7 @@ public class CameraActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         setContentView(R.layout.activity_camera);
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (!Settings.canDrawOverlays(this.getApplicationContext())) {
@@ -52,6 +62,7 @@ public class CameraActivity extends Activity {
                     .replace(R.id.container, CameraConnectionFragment.newInstance())
                     .commit();
         }
+
     }
 
     @Override
@@ -67,5 +78,28 @@ public class CameraActivity extends Activity {
                 }
             }
         }
+    }
+
+    public void OnclickHandler(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("졸음으로 인식되었습니다.").setMessage("하품이 맞습니까?");
+
+        builder.setPositiveButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(getApplicationContext(), "NO Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(getApplicationContext(), "OK Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
